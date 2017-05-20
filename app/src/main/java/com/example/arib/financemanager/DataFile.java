48634352f6cac3e52e9a9d9a7c1af8f89816623a
Arib <atxarib99@gmail.com>
2017-05-20 14:53:31 -0500

    removed unused imports and removed the local text parser from the Data File class
    I moved the method that parses Strings back into an ArrayList of
    expenses into the MainActivity because it was a centralized place that
    could handle it. Instead of having each class contain a text parser, I
    moved it into the MainActivity as a static method and made it smarter to
    detect when Strings cannot be parsed into ArrayLists because it is now a
    possibility.

--- app/src/main/java/com/example/arib/financemanager/DataFile.java
+++ app/src/main/java/com/example/arib/financemanager/DataFile.java
@@ -1,10 +1,7 @@
 package com.example.arib.financemanager;
 
-import android.util.Log;
-
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created by Arib on 12/12/2016.
@@ -26,25 +23,6 @@ class DataFile implements Serializable {
 
     public void setFilename(String filename) {
         this.filename = filename;
-    }
-
-    ArrayList<Expenses> getExpenses() {
-        String finalString = textualExpenses.replace("[", "");
-        finalString = finalString.replace("]", "");
-        String[] arrayToConvert = finalString.split(", ");
-        ArrayList<Expenses> returnable = new ArrayList<>();
-        for(String s : arrayToConvert) {
-            String[] indivExpense = s.split("-");
-            Log.d(LOG_TAG, returnable + "");
-            Log.d(LOG_TAG, Arrays.toString(indivExpense));
-            String title = indivExpense[0];
-            double amount = Double.parseDouble(indivExpense[1]);
-            String category = indivExpense[2];
-            String date = indivExpense[3];
-            returnable.add(new Expenses(title, amount, category, date));
-        }
-
-        return returnable;
     }
 
     public String toString() {
